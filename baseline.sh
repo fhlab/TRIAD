010e3f65d79896444b9556a88f281ce681abf26b
MayaPetek <mayapetek1@gmail.com>
2019-07-29 15:48:06 +0100

    Baseline

--- baseline.sh
+++ baseline.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 ../baseline.py S6short.fa 
 needleall -gapopen 15 -gapextend 0.5 -asequence S6short.fa -supper1 -bsequence S6.d_baseline.fa -supper2 -aformat3 fasta -outfile S6.d_baseline.aln
+../PTE_composition.py -f . -b N -s 3 -e 3 -o d_baseline
